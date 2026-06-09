bb9dec5516d271805349284c2e605a492bd761b4
nivethitha <mnivethitha369@gmail.com>
2026-06-09 11:24:50 +0530

    first

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ const observer = new IntersectionObserver(entries => {
 document.querySelectorAll(".hidden").forEach(el => {
     observer.observe(el);
 });
+
+const scriptURL =
+"https://script.google.com/macros/s/AKfycbzuvME_ehxd08WxoDE51LOFHS2AlgIa-IzX3qt35n77ljcy7WZ8SX4Fyn3SHnV9K90gIA/exec";
+
+function toggleMenu(){
+    document
+        .querySelector(".contact-menu")
+        .classList.toggle("active");
+}
