e7cc5d20abaa9a17ed20ac82e71256539a40ff79
nivethitha <mnivethitha369@gmail.com>
2026-06-12 14:56:53 +0530

    first

--- js/script.js
+++ js/script.js
@@ -10,11 +10,19 @@ document.querySelectorAll(".hidden").forEach(el => {
     observer.observe(el);
 });
 
-const scriptURL =
-"https://script.google.com/macros/s/AKfycbzuvME_ehxd08WxoDE51LOFHS2AlgIa-IzX3qt35n77ljcy7WZ8SX4Fyn3SHnV9K90gIA/exec";
-
 function toggleMenu(){
     document
         .querySelector(".contact-menu")
         .classList.toggle("active");
+}
+
+function toggleNav(){
+
+    document
+        .getElementById("navMenu")
+        .classList.toggle("active");
+
+    document
+        .getElementById("hamburger-btn")
+        .classList.toggle("open");
 }
